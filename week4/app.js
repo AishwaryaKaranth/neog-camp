3a24318b66d604333600e2b4588ebdf1dfc1eab3
Aishwarya Karanth <35144851+AishwaryaKaranth@users.noreply.github.com>
2020-12-10 16:55:16 +0530

    Create app.js
    food emoji translator

---
+++ week4/app.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { useState } from "react";
+import "./styles.css";
+
+const emojis = {
+  "🍕": "Pizza",
+  "🍔": "Burger",
+  "🌭": "Hot Dog",
+  "🍿": "PopCorn",
+  "🌮": "Taco",
+  "🥯": "Bagel",
+  "🥟": "Dumpling",
+  "🍰": "Cake",
+  "🥨": "Pretzel",
+  "🧇": "Waffles"
+};
+
+const emojiDict = Object.keys(emojis);
+export default function App() {
+  const [result, setResult] = useState("");
+
+  function inputHandler(event) {
+    var userInput = event.target.value;
+    var result = emojis[userInput];
+    setResult(result);
+  }
+  function emojiInputHandler(emoji) {
+    var result = emojis[emoji];
+    setResult(result);
+  }
+  return (
+    <div className="App">
+      <h1>Food emoji translator!</h1>
+
+      <input
+        onChange={inputHandler}
+        placeholder="Type in an food emoji here"
+      ></input>
+      <h3>{result}</h3>
+      {emojiDict.map(function (emoji) {
+        return (
+          <span
+            style={{ padding: "0.5rem", fontSize: "2.5rem", cursor: "pointer" }}
+            key={emoji}
+            onClick={() => emojiInputHandler(emoji)}
+          >
+            {emoji}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
