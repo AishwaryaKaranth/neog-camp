201cbdeb6daa322f8b5911afbf08e880c99544c1
AishwaryaKaranth <aishwaryakaranth27899@gmail.com>
2020-12-11 21:50:23 +0530

    update index.html

--- week-3/translator1/app.js
+++ week-3/translator1/app.js
@@ -1,2 +1,23 @@
-var translate=document.querySelector("btn-translate")
-var input=textarea.value
+var btnTranslate= document.querySelector("#btn-translate")
+var inputText=document.querySelector("#txt-input")
+var outputText=document.querySelector("#output")
+var server="https://api.funtranslations.com/translate/mandalorian.json"
+
+function translateText(text){
+    return server+"?"+"text="+text
+}
+function errorHandler(error){
+    console.log("error occured:",error)
+}
+function clickHandler(){
+    var output=inputText.value;
+    fetch(translateText(output))
+    .then(response=>response.json())
+    .then(json=>{
+        var translatedText = json.contents.translated;
+        outputText.innerText=translatedText;
+    })
+    .catch(errorHandler)
+}
+btnTranslate.addEventListener("click", clickHandler)
+
