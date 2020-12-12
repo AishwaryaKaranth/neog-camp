6a5ef1277d6358e912871e5231a1e5640deb7b03
Aishwarya Karanth <35144851+AishwaryaKaranth@users.noreply.github.com>
2020-12-12 22:47:34 +0530

    Update index.js

--- week-1/cli-app-1/index.js
+++ week-1/cli-app-1/index.js
@@ -1,1 +1,56 @@
+var readlineSync=require('readline-sync')
+var chalk=require('chalk');
+var userName=readlineSync.question("Hi there!, What's your name?");
+
+console.log(chalk.bold.bgCyan("Welcome "+userName+" to 'How well do you know me?'."));
+console.log(chalk.magenta("Let's Play!"));
+
+var score=0;
+function quiz(question,answer){
+  var userResponse=readlineSync.question(question);
+  if(userResponse==answer){
+    score++;
+    console.log(chalk.green("That is correct!"));
+    console.log("Your current score is ",score);
+  }
+  else{
+    console.log(chalk.red("That is incorrect. The correct answer is ",answer));
+  }
+}
+var quizQuestions=[
+  {
+    question:'"Where do I live?"',
+    answer:'Bangalore'
+  },
+  {
+    question:'"What do I major in at University?"',
+    answer:'Electronics and communication Engineering'
+  },
+  {
+    question:'"Do I have a pet?"',
+    answer:'Yes'
+  },
+  {
+    question:'"What pet do I have?"',
+    answer:'Cat'
+  }
+]
+var highscore = [{
+  name: "Aparna",
+  score: 3},
+{ name: "Adhya",
+  score: 1 },
+{ name: "Aisiri",
+  score: 1 }]
+
+for(var i=0;i<4;i++){
+  var current=quizQuestions[i];
+  quiz(current.question, current.answer);
+}
+
+console.log('*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*');
+
+  console.log(chalk.bgCyan("Your final score is "+score+" !"));
+  console.log("Highest Score:");
+  console.log(highscore[0].name, highscore[0].score)
 
